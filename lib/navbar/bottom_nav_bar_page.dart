5d1645c119c1c8b772bd80834d12db676da2712d
wisnuibroo <wisnuaja0999@gmai.com>
2025-04-10 09:47:00 +0700

    Feat:[WF] pesan-binding-controller

--- lib/navbar/bottom_nav_bar_page.dart
+++ lib/navbar/bottom_nav_bar_page.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:laundryku/favorite/pages/Favorite_page.dart';
 import 'package:laundryku/home/pages/home_page.dart';
 import 'package:laundryku/navbar/bottom_nav_controller.dart';
+import 'package:laundryku/pesan/pages/pesan_page.dart';
 import 'package:laundryku/profile/pages/profile_page.dart';
 
 class BottomNavBarPage extends StatefulWidget {
@@ -18,6 +19,7 @@ class _NavbarPageState extends State<BottomNavBarPage> {
   final List<Widget> _menus = [
     HomePage(),
     FavoritePage(),
+    PesanPage(),
     ProfilePage(),
   ];
 
