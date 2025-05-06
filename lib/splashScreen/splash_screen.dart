17e45b6d4f651e6b25751910d1b974243f675cb5
ikanbil <abiljr313@gmail.com>
2025-05-06 08:00:23 +0700

    Feat:[KN] Add Shared Prefence

--- lib/splashScreen/splash_screen.dart
+++ lib/splashScreen/splash_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:laundryku/widget/my_text.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,15 +12,27 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(const Duration(seconds: 3), () {
-      if (mounted) {
-        Get.toNamed('/login');
-      }
-    });
+@override
+void initState() {
+  super.initState();
+  _checkLoginStatus();
+}
+
+Future<void> _checkLoginStatus() async {
+  final prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString('token');
+
+  await Future.delayed(Duration(seconds: 3));
+
+  if (!mounted) return;
+
+  if (token != null && token.isNotEmpty) {
+    Get.offAllNamed('/navbar');
+  } else {
+    Get.offAllNamed('/login');
   }
+}
+
 
   @override
   Widget build(BuildContext context) {
